15b3b6d50310a5ea562aadb4dc3bee5982153724
Lucas Diniz <lucdinizcontato@gmail.com>
2024-03-27 22:30:07 -0300

    Salvar avaliação apenas com um usuário logado

--- src/main/java/com/dnz/lojaweb/controller/AvaliacoesController.java
+++ src/main/java/com/dnz/lojaweb/controller/AvaliacoesController.java
@@ -3,6 +3,8 @@ package com.dnz.lojaweb.controller;
 import com.dnz.lojaweb.model.AvaliacoesEntity;
 import com.dnz.lojaweb.model.ProdutoEntity;
 import com.dnz.lojaweb.repository.AvaliacoesService;
+import com.dnz.lojaweb.utils.SessionManager;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -15,10 +17,17 @@ public class AvaliacoesController {
     @Autowired
     AvaliacoesService as;
     
+    @Autowired
+    SessionManager sm;
+    
     @PostMapping("/registerAnalise")
-    public String revRegister(@ModelAttribute("analise") AvaliacoesEntity avaliacao, @ModelAttribute("produto") ProdutoEntity produto){
-        avaliacao.setProduct(produto);
-        as.saveAvaliacao(avaliacao);
-        return "redirect:/product/detalhes/" + avaliacao.getProduct().getId();
+    public String revRegister(@ModelAttribute("analise") AvaliacoesEntity avaliacao, @ModelAttribute("produto") ProdutoEntity produto, HttpSession session){
+        if(sm.isUserLogged(session)){
+            avaliacao.setProduct(produto);
+            as.saveAvaliacao(avaliacao);
+            return "redirect:/product/detalhes/" + avaliacao.getProduct().getId();
+        }else{
+            return "redirect:/usuario/login";
+        }
     }
 }
